ee37c86ea6464da718c81b74dff9ac56e2852dbd
CountBleck <Mr.YouKnowWhoIAm@protonmail.com>
2023-01-25 21:15:59 -0800

    Add CHE tests pertaining to header name IDs and strings
    The greater test coverage we achieve over CHE, the better it will become
    as a whole.

--- compact-header-encoding/tests.mjs
+++ compact-header-encoding/tests.mjs
@@ -27,19 +27,66 @@ ensureUnchanged([
     ["Content-Type", "text/html"],
 ])
 
-let i = 0
-let error
-for (;;) {
-    try {
-        ensureUnchanged([["foo", "A".repeat(i)]], false)
-    } catch (e) {
-        error = e
-        break
+{
+    let i = 0
+    let error
+    for (;;) {
+        try {
+            ensureUnchanged([["foo", "A".repeat(i)]], false)
+        } catch (e) {
+            error = e
+            break
+        }
+        i++
     }
-    i++
+
+    equal(error.message, "Header value length exceeds maximum length", error)
+    equal(i, 212110 + 1, "Error was thrown on an unexpected header value length")
 }
 
-equal(error.message, "Header value length exceeds maximum length", error)
-equal(i, 212110 + 1, "Error was thrown on an unexpected header value length")
+{
+    let i = 0
+    let error
+    for (;;) {
+        try {
+            ensureUnchanged([[i, "foo"]], false)
+        } catch (e) {
+            error = e
+            break
+        }
+        i++
+    }
+
+    equal(error.message, "Header ID exceeds maximum ID", error)
+    equal(i, 8929 + 1, "Error was thrown on an unexpected header ID")
+}
+
+{
+    let error
+    try {
+        compactEncode([["", "foo"]])
+    } catch (e) {
+        error = e
+    }
+
+    equal(error.message, "Header name must not be empty", error)
+}
+
+{
+    let i = 1
+    let error
+    for (;;) {
+        try {
+            ensureUnchanged([["A".repeat(i), "foo"]], false)
+        } catch (e) {
+            error = e
+            break
+        }
+        i++
+    }
+
+    equal(error.message, "Header name length exceeds maximum length", error)
+    equal(i, (94 + 1) + 1, "Error was thrown on an unexpected header name length")
+}
 
 // TODO: More tests
